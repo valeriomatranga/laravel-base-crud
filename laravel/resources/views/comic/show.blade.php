6d995f46b0dead64a80f1e456c1c5453d2a9f228
valeriomatranga <78118886+valeriomatranga@users.noreply.github.com>
2021-07-15 18:01:00 +0200

    implementato destroy comics

--- laravel/resources/views/comic/show.blade.php
+++ laravel/resources/views/comic/show.blade.php
@@ -5,19 +5,26 @@
 @section('content')
 
     <div class="flex">
-        <div class="update">
+        <div class="button">
             <a href="{{route('comics.edit', $comic->id)}}">modifica fumetto</a>
         </div>
-        <div class="back">
+        <div class="button">
             <a href="{{route('comics.index')}}">Torna alla home</a>
         </div>
-        
+        <div>
+            <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                @csrf
+                @method('DELETE')
+                <input style="color: red;" class="button" type="submit" value="Cancella">
+            </form>
+        </div>
         <h2>{{$comic->titolo}}</h2>
         <img class="poster" src="{{$comic->poster}}" alt="">
         <div>
             <span><strong>Anno di uscita:</strong> {{$comic->anno}}</span><br>
             <span><strong>Prezzo:</strong> {{$comic->prezzo}} Euro</span>
         </div>
+
     </div>
 
 @endsection
